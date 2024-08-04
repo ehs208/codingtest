932fe650ba24dbb040fedfddb862d8b04af20628
EunHyunsu <109217208+ehs208@users.noreply.github.com>
2024-08-04 12:37:47 +0900

    [level 0] Title: 두 수의 합, Time: 476.23 ms, Memory: 115 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,14 @@
+import java.math.*;
+
+class Solution {
+    public String solution(String a, String b) {
+        
+        BigInteger bigNumber1 = new BigInteger(a);
+        BigInteger bigNumber2 = new BigInteger(b);
+        
+        BigInteger sum = bigNumber1.add(bigNumber2);
+        
+        String answer = sum.toString();
+        return answer;
+    }
+}
